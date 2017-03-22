4790f86cee4dee19682b302a02e3abd9d8f60498
Stegen Smith <matthsmi@adobe.com>
2017-03-22 16:46:06 +0000

    more tweaks

--- haproxy/scripts/deploy.sh
+++ haproxy/scripts/deploy.sh
@@ -2,4 +2,15 @@
 
 # yum -y update
 yum -y install epel-release
-yum -y install haproxy
+yum -y install haproxy wget curl sssd ca-certificates
+systemctl enable haproxy
+
+curl -k https://10.13.98.8/ztp/loadtest_slash.tar.gz > /tmp/loadtest_slash.tar.gz
+
+cd /
+tar -zxvf /tmp/loadtest_slash.tar.gz
+
+systemctl restart sssd
+systemctl restart sshd
+
+exit 0
