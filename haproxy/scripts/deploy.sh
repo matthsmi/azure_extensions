d5eed2e8871d80f931bd1691db40d734eb8a5c6a
Stegen Smith <matthsmi@adobe.com>
2017-03-23 15:22:56 +0000

    updated script

--- haproxy/scripts/deploy.sh
+++ haproxy/scripts/deploy.sh
@@ -5,20 +5,35 @@ export PATH
 
 # yum -y update
 yum -y install epel-release
-yum -y install haproxy wget curl sssd ca-certificates
-systemctl enable haproxy
-
-curl -k https://10.13.98.8/ztp/loadtest_slash.tar.gz > /tmp/loadtest_slash.tar.gz
+yum -y install wget curl sssd ca-certificates
 
 cd /
-tar -zxvf /tmp/loadtest_slash.tar.gz
+
+if [ ! -d /opt ]; then
+    mkdir /opt ; chmod 755 /opt
+fi
+
+FILES="loadtest_slash.tar.gz opt_haproxy.tar.gz"
+for file in $FILES
+do
+    curl -k https://10.13.98.8/ztp/$file > /tmp/$file
+    tar -zxvf /tmp/$file
+    rm -f /tmp/$file
+done
+
+curl -k https://10.13.98.8/ztp/haproxy.cfg /opt/haproxy/etc
+curl -k https://10.13.98.8/ztp/haproxy-sysctl.conf /etc/sysctl.d/haproxy.conf
+
+cp /opt/haproxy/systemd/haproxy.service /etc/systemd/system/haproxy.service
 
 SERVICES="sssd sshd rsyslog haproxy"
 for service in $SERVICES
 do
+    systemctl enable $service
     systemctl restart $service
 done
 
 setenforce 0
+sysctl -p /etc/sysctl.d/haproxy.conf
 
 exit 0
