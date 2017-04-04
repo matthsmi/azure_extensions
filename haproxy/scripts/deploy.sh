e0fc62e21921f2f9e6601f798ebe965349286311
Stegen Smith <matthsmi@adobe.com>
2017-04-04 10:20:39 +0100

    updated deploy.sh

--- haproxy/scripts/deploy.sh
+++ haproxy/scripts/deploy.sh
@@ -3,6 +3,18 @@
 PATH=/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin
 export PATH
 
+### BEGIN AWFUL FIXUPS
+# 
+cp /etc/resolv.conf /etc/resolv.conf.bak
+cat << EOF >> /etc/resolv.conf 
+nameserver 8.8.8.8
+EOF
+
+# I've seen the olcentgbl has a lot of 404 for files, and that screws the deployment
+cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+sed -i 's/olcentgbl\.trafficmanager\.net/mirrors\.kernel\.org/g' /etc/yum.repos.d/CentOS-Base.repo
+### END AWFUL FIXUPS
+
 # yum -y update
 yum -y install epel-release
 yum -y install wget curl sssd ca-certificates yum-utils
@@ -14,7 +26,10 @@ cd /
 
 if [ ! -d /opt ]; then
     mkdir /opt ; chmod 755 /opt
-    mkdir /config ; chmod 755 /config
+fi
+
+if [ ! -d /config ]; then
+    mkdir -p /config/certs /config/conf ; chmod 755 /config /config/conf /config/certs
 fi
 
 #FILES="loadtest_slash.tar.gz opt_haproxy.tar.gz"
@@ -23,25 +38,32 @@ for file in $FILES
 do
     curl -k https://10.13.98.8/ztp/$file > /tmp/$file
     tar -zxvf /tmp/$file
-    rm -f /tmp/$file
+    #rm -f /tmp/$file
 done
 
 #curl -k https://10.13.98.8/ztp/haproxy.cfg > /opt/haproxy/etc
 #curl -k https://10.13.98.8/ztp/haproxy-sysctl.conf > /etc/sysctl.d/haproxy.conf
 #cp /opt/haproxy/systemd/haproxy.service /etc/systemd/system/haproxy.service
-curl -k https://10.13.98.8/ztp/docker-haproxy.service > /etc/sytemd/system/docker-haproxy.service
-curl -k https://10.13.98.8/ztp/haproxy.cfg > /config
+
+curl -k https://10.13.98.8/ztp/haproxy-sysctl.conf > /etc/sysctl.d/haproxy.conf
+curl -k https://10.13.98.8/ztp/docker-haproxy.service > /etc/systemd/system/docker-haproxy.service
+curl -k https://10.13.98.8/ztp/haproxy.cfg > /config/conf/haproxy.cfg
+curl -k https://10.13.98.8/ztp/admin_ethos-adobe-net.pem > /config/certs/admin_ethos-adobe-net.pem
+curl -k https://10.13.98.8/ztp/web_wa1dev-ethos-adobe-net.pem > /config/certs/web_wa1dev-ethos-adobe-net.pem
 
 setenforce 0
 sysctl -p /etc/sysctl.d/haproxy.conf
 
 #SERVICES="sssd sshd rsyslog haproxy"
 
-#SERVICES="sssd sshd rsyslog docker docker-haproxy.service"
-#for service in $SERVICES
-#do
-#    systemctl enable $service
-#    systemctl restart $service
-#done
+SERVICES="sssd sshd rsyslog docker docker-haproxy.service"
+for service in $SERVICES
+do
+    systemctl enable $service
+    systemctl restart $service
+done
 
+
+mv /etc/resolv.conf.bak /etc/resolv.conf
+mv /etc/yum.repos.d/CentOS-Base.repo.bak /etc/yum.repos.d/CentOS-Base.repo
 exit 0
